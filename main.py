8efaae983db02707daefa1b87a37f8160e0688ec
Taelyth <jaquebeneduzzi@hotmail.com>
2021-09-22 22:19:05 -0300

    Inclusão da Calculadora e dos testes unitários

---
+++ main.py
@@ -0,0 +1,85 @@
+# 1 - imports - bibliotecas
+
+
+# 2 - class - classe
+
+# 3 - definitions - definições = métodos e funções
+def print_hi(name):
+    print(f'Oi, {name}')  # O f serve para chamar o parâmetro da função
+
+
+def somar(numero1, numero2):
+    return numero1 + numero2
+
+
+def subtrair(numero1, numero2):
+    return numero1 - numero2
+
+
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+
+
+def dividir(numero1, numero2):
+    if numero2 != 0:
+        return numero1 // numero2
+    else:
+        return 'Não pode dividir por Zero'
+
+
+# Testes Unitários / Teste de Unidades
+
+# teste da função somar
+def test_somar():
+    # 1 - Configura / Prepara
+    numero1 = 8  # input / entrada
+    numero2 = 5  # input / entrada
+    resultado_esperado = 13  # output / saída
+
+    # 2 - Executa
+    resultado_atual = somar(numero1, numero2)
+
+    # 3 - Checa / Valida
+    assert resultado_atual == resultado_esperado
+    print(f'\nO resultado atual é: {resultado_atual}\nO resultado esperado é: {resultado_esperado}')
+
+
+# o mesmo teste, mas resumido
+def test_somar_compacto():
+    assert somar(8, 5) == 13
+
+
+def test_subtrair_compacto():
+    assert subtrair(4, 5) == -1
+
+
+def test_multiplicar_compacto():
+    assert multiplicar(4, 3) == 12
+
+
+def test_dividir_compacto():
+    assert dividir(8, 2) == 4
+
+
+def test_dividir_por_zero_compacto():
+    assert dividir(8, 0) == 'Não pode dividir por Zero'
+
+
+if __name__ == '__main__':
+    print_hi('Jaque')
+
+    # soma de 2 números
+    resultado = somar(1, 2)
+    print(f'O resultado da soma é: {resultado}')
+
+    # subtração de 2 números
+    resultado = subtrair(5, 3)
+    print(f'O resultado da subtração é: {resultado}')
+
+    # multiplicação
+    resultado = multiplicar(2, 4)
+    print(f'O resultado da multiplicação é: {resultado}')
+
+    # divisão
+    resultado = dividir(4, 2)
+    print(f'O resultado da divisão é: {resultado}')
